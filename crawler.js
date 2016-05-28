d4f47ad32dbed71dd9aa578766ab45912a0562a9
Amila Welihinda <amilajack@gmail.com>
2016-05-28 12:39:23 -0700

    Added initial boilerplate for project and crawler

---
+++ crawler.js
@@ -0,0 +1,28 @@
+const Nightmare = require('nightmare');
+const nightmare = new Nightmare({ show: true });
+
+/**
+ * Return a list of stock symbols gathered from yahoo
+ *
+ * @param  {number} limit
+ * @return {array}
+ */
+function getStockSymbols() {
+
+}
+
+/**
+ * Filter stocks from the
+ */
+function filterUnique() {
+
+}
+
+/**
+ * Initiate crawling of yahoo
+ */
+function crawl() {
+
+}
+
+module.exports = { crawl, filterUnique, getStockSymbols };
