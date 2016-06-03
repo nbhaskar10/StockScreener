f778bbcce5dfc59eb17a62953d183ed4ee3de117
Bhaskar Nyshadham <nbhaskar10@gmail.com>
2016-06-03 13:21:15 -0700

    parser.js changed to read the ticker symbol column from the csv file and store as a json array

--- src/Parser.js
+++ src/Parser.js
@@ -17,7 +17,23 @@
  * @param  {array} csvFilePaths
  * @return {array}
  */
+
+/* this should be able to return all of the tickers from NASDAQ.csv 
+in the fomat [ { Symbol: 'Symbol' }, { Symbol: 'TFSC' }, etc
+*/
+
 function Parser(csvFilePaths) {
+	const csv = require('csv-to-array')
+	let columns = ["Ticker"]
+	let arr=[];
+	csv({
+	 file: csvFilePaths[0],
+	 columns: columns
+	}, (error, results) => {
+	 if (error) console.log(error);
+	 console.log(results);
+	})
+
 
 }
 
