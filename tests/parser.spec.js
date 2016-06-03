2b123fc6b8f85b63ad52701d9b00d8abcd7afb66
Amila Welihinda <amilajack@gmail.com>
2016-06-03 15:02:20 -0700

    Improved test coverage to 100%

--- tests/parser.spec.js
+++ tests/parser.spec.js
@@ -4,19 +4,23 @@ const Risk         = require('../src/RiskCalculator')
 const Favorability = require('../src/FavorabilityCalculator')
 
 
-const parsedCsvFiles = Parser([
-  '../csv/NASDAQ.csv',
-  '../csv/NYSE.csv'
-])
+Parser([
+  './csv/NASDAQ.csv',
+  './csv/NYSE.csv'
+], (parsedCsvFiles) => {
+  // Assert types
+  assert.equal(Array.isArray(parsedCsvFiles), true)
+  assert.equal(typeof parsedCsvFiles[0] === 'object', true)
 
-// Assert types
-assert.equal(Array.isArray(parsedCsvFiles), true)
-assert.equal(typeof parsedCsvFiles[0] === 'object', true)
+  // Assert has properties
+  const stock = parsedCsvFiles[0]
+  const requiredProperties = ['Symbol', 'Name', 'LastSale', 'MarketCap', 'Sector']
 
-// Assert has properties
-const stock = parsedCsvFiles[0]
-const requiredProperties = ['symbol', 'name', 'lastSale', 'marketCap', 'sector']
-
-for (const property of requiredProperties) {
-  assert.equal(typeof stock[property] === 'string', true)
-}
+  for (const property of requiredProperties) {
+    console.log(property, stock[property]);
+    assert.equal(
+      typeof stock[property] === 'string' ||
+      typeof stock[property] === 'number', true
+    )
+  }
+})
